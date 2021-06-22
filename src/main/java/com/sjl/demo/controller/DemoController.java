f226a645e4616504110538c0aeb9744bbdaf8709
18218725800 <Omaiga123>
2021-06-22 10:19:01 +0800

    jenkins测试bbbbb

--- src/main/java/com/sjl/demo/controller/DemoController.java
+++ src/main/java/com/sjl/demo/controller/DemoController.java
@@ -8,6 +8,6 @@ public class DemoController {
 
     @RequestMapping("/hello")
     public String hello(){
-        return "HELLO word,test...bbbbbb";
+        return "HELLO word,test...bbbbbbb";
     }
 }
